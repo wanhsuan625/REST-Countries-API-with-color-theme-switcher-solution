df06ad9c0f2e2fdfea77cc05a4e88fea21d858e1
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2024-03-06 16:29:53 +0800

    Feat: add footer on the bottom on each page and modify the margin bottom for Error.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './sections/Header';
 import HomePage from './sections/HomePage';
 import Country from './sections/Country';
+import Footer from './sections/Footer';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path='/country/:countryName' element={<Country/>} />        {/* :country 動態路徑 */}
       </Routes>
+
+      <Footer/>
     </BrowserRouter>
   </>
   );
---
+++ src/sections/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer () {
+    return(
+    <>
+        <section className='h-20 border-t-2 font-medium text-sm flex justify-center items-center md:text-base'>
+            <span>Challenge by <a href='https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca/hub' target='_blank' rel="noreferrer" className='cursor-pointer underline'>Frontend Mentor</a>.
+                Code by <a href='https://github.com/wanhsuan625' target='_blank' rel="noreferrer" className='underline cursor-pointer'>Bonnie</a>.
+            </span>
+        </section>
+    </>
+    )
+}
+
+export default Footer;
